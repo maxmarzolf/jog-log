f7be3dae3a44ec59f941d9b2b6be2ad209d57b89
maxmarzolf <max@treesfromspace.com>
2020-01-22 20:10:47 -0500

    simple

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-#running app
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+@app.route('/', methods=['GET', 'POST'])
+def miles_ran():
+    print(request.method)
+    if request.method == 'GET':
+        return render_template('test.html')
+    elif request.method == 'POST':
+        miles = request.form['mile_input']
+        return "miles are {}".format(miles)
+    else:
+        return render_template('test.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
