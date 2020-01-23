8af958ab9ff3620cc5bc87bf21d57ead6529bae3
maxmarzolf <max@treesfromspace.com>
2020-01-23 18:43:10 -0500

    initial database

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request
-
+from datetime import datetime
+import sqlite3
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def miles_ran():
-    print(request.method)
     if request.method == 'GET':
         return render_template('test.html')
     elif request.method == 'POST':
         miles = request.form['mile_input']
+        insert_into_table(miles)
         return "miles are {}".format(miles)
     else:
         return render_template('test.html')
+
+
+def insert_into_table(miles):
+    current_date = datetime.now()
+    conn = sqlite3.connect('data')
+    c = conn.cursor()
+    c.execute('insert into run values (current_date, \"%s\")' % miles)
+    conn.commit()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
