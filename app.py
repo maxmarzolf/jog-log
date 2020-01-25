e4f4bacbe571a6127f3cb55d3c2272c9f70dca54
maxmarzolf <max@treesfromspace.com>
2020-01-25 15:33:19 -0500

    simple graph with bad logic flow

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 from datetime import datetime
+from bokeh.plotting import figure, output_file, show
+import pandas as pd
+from sqlalchemy import create_engine
 import sqlite3
+
 app = Flask(__name__)
 
 
@@ -20,9 +24,20 @@ def insert_into_table(miles):
     current_date = datetime.now()
     connection = sqlite3.connect('data')
     cursor = connection.cursor()
-    cursor.execute('insert into run values (current_date, \"%s\")' % miles)
+    cursor.execute('insert into run values (\"%s\")' % miles)
     connection.commit()
+    show_graph()
 
+
+def show_graph():
+    engine = create_engine('sqlite:///data')
+    dataframe = pd.read_sql('select * from run', engine)
+    x_axis = [1, 2, 3, 4, 5, 6, 7, 8]
+    y_axis = dataframe.values.tolist()
+    output_file("templates/graph.html")
+    p = figure(title="Miles Ran", x_axis_label='day', y_axis_label='miles')
+    p.line(x_axis, y_axis, legend_label="Miles per Day", line_width=2)
+    show(p)
 
 if __name__ == '__main__':
     app.run(debug=True)
