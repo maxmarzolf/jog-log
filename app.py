c987885b50aa4e8d460a8ef44ae9fdfb678ac705
maxmarzolf <max@treesfromspace.com>
2020-01-26 13:26:15 -0500

    improved logic flow, x_axis graph auto incremented

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
-from datetime import datetime
-from bokeh.plotting import figure, output_file, show
+import sqlite3
 import pandas as pd
 from sqlalchemy import create_engine
-import sqlite3
+from flask import Flask, render_template, request
+from bokeh.plotting import figure, output_file, show
+
 
 app = Flask(__name__)
 
@@ -11,33 +11,39 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def miles_ran():
     if request.method == 'GET':
-        return render_template('test.html')
+        return render_template('main.html')
     elif request.method == 'POST':
         miles = request.form['mile_input']
         insert_into_table(miles)
-        return "miles are {}".format(miles)
-    else:
-        return render_template('test.html')
+        return render_template('main.html')
 
 
 def insert_into_table(miles):
-    current_date = datetime.now()
-    connection = sqlite3.connect('C:\\Users\\bmbur\\dev\\jog-log\\data')
+    connection = sqlite3.connect('data')
     cursor = connection.cursor()
-    cursor.execute('insert into run values (\"%s\")' % miles)
+    cursor.execute('insert into run (miles_ran) values (\"%s\")' % miles)
     connection.commit()
-    show_graph()
+    cursor.close()
+    connection.close()
 
 
 def show_graph():
     engine = create_engine('sqlite:///data')
     dataframe = pd.read_sql('select * from run', engine)
-    x_axis = [1, 2, 3, 4, 5, 6, 7, 8]
-    y_axis = dataframe.values.tolist()
+    x_axis = dataframe['Day'].tolist()
+    y_axis = dataframe['Miles_Ran'].tolist()
+    print(x_axis)
     output_file("templates/graph.html")
-    p = figure(title="Miles Ran", x_axis_label='day', y_axis_label='miles')
+    p = figure(title="Miles Ran", x_axis_label='day', y_axis_label='miles', height=375, width=1200)
     p.line(x_axis, y_axis, legend_label="Miles per Day", line_width=2)
     show(p)
+
+
+@app.route("/graph.html", methods=['POST'])
+def move_forward():
+    show_graph()
+    return render_template('graph.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
