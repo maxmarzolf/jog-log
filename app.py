e209f608679da85e2809dd1cc3e342769b90e96e
maxmarzolf <max@treesfromspace.com>
2020-02-01 20:58:05 -0500

    modified graph display

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sqlite3
 import pandas as pd
 from sqlalchemy import create_engine
 from flask import Flask, render_template, request
+from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, output_file, show
 
 
@@ -15,11 +16,12 @@ def miles_ran():
     elif request.method == 'POST':
         miles = request.form['mile_input']
         insert_into_table(miles)
+        show_graph()
         return render_template('main.html')
 
 
 def insert_into_table(miles):
-    connection = sqlite3.connect('C:\\Users\\bmbur\\dev\\jog-log\\data')
+    connection = sqlite3.connect('data')
     cursor = connection.cursor()
     cursor.execute('insert into run (miles_ran) values (\"%s\")' % miles)
     connection.commit()
@@ -28,20 +30,17 @@ def insert_into_table(miles):
 
 
 def show_graph():
-    engine = create_engine('sqlite:///C:\\Users\\bmbur\\dev\\jog-log\\data')
-    dataframe = pd.read_sql('select * from run', engine)
-    x_axis = dataframe['Day'].tolist()
-    y_axis = dataframe['Miles_Ran'].tolist()
-    output_file("graph.html")
-    p = figure(title="Miles Ran", x_axis_label='day', y_axis_label='miles', height=375, width=1200)
-    p.line(x_axis, y_axis, legend_label="Miles per Day", line_width=2)
-    show(p)
-
-
-@app.route("/graph.html", methods=['POST'])
-def move_forward():
-    show_graph()
-    return render_template('graph.html')
+    engine = create_engine('sqlite:///data')
+    df = pd.read_sql('select * from run', engine)
+    x_axis = df['Day'].tolist()
+    y_axis = df['Miles_Ran'].tolist()
+    df2 = df[1:]
+    ColumnDataSource(df2)
+    output_file("templates/graph.html")
+    p = figure(title="Miles Ran", x_axis_label='day', y_axis_label='miles', height=225, width=950,
+               toolbar_location='above')
+    p.background_fill_color = '#f4f4f4'
+    p.line(x_axis, y_axis, legend_label="Miles per Day", line_width=2, line_color="#000000")
 
 
 if __name__ == '__main__':
