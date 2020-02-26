7597a946d0eb7654919b3568e49f1115088737ee
maxmarzolf <max@treesfromspace.com>
2020-02-25 22:04:03 -0500

    blueprint started

---
+++ views/home.py
@@ -0,0 +1,44 @@
+import flask
+from flask import render_template, request
+import sqlite3
+import pandas as pd
+from sqlalchemy import create_engine
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure, output_file, show
+
+blueprint = flask.Blueprint('home', __name__)
+
+
+@blueprint.route('/', methods=['GET', 'POST'])
+def miles_ran():
+    if request.method == 'GET':
+        return render_template('home.html')
+    elif request.method == 'POST':
+        miles = request.form['mile_input']
+        insert_into_table(miles)
+        show_graph()
+        return render_template('home.html')
+
+
+def insert_into_table(miles):
+    connection = sqlite3.connect('data')
+    cursor = connection.cursor()
+    cursor.execute('insert into run (miles_ran) values (\"%s\")' % miles)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def show_graph():
+    engine = create_engine('sqlite:///data')
+    df = pd.read_sql('select * from run', engine)
+    x_axis = df['Day'].tolist()
+    y_axis = df['Miles_Ran'].tolist()
+    df2 = df[1:]
+    ColumnDataSource(df2)
+    output_file("templates/graph.html")
+    p = figure(title="Miles Ran", x_axis_label='day', y_axis_label='miles', height=225, width=950,
+               toolbar_location='above')
+    p.background_fill_color = '#f4f4f4'
+    p.line(x_axis, y_axis, legend_label="Miles per Day", line_width=2, line_color="#000000")
+    show(p)
